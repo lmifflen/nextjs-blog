e31841c90734727fef84f3323828e2e562ff0485
Luke-NIM <luke@newideamachine.com>
2023-03-25 19:18:22 -0600

    pre-rendering and datafetching lesson

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,50 @@
-import Head from 'next/head';
-import Layout, { siteTitle } from '../components/layout';
-import utilStyles from '../styles/utils.module.css';
+import Head from "next/head";
+import Layout, { siteTitle } from "../components/layout";
+import utilStyles from "../styles/utils.module.css";
 
-export default function Home() {
+import { getSortedPostsData } from "../lib/posts";
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Hello I'm <bold>Luke</bold> A software developer from Calgary. You can contact me on <a href='https://www.linkedin.com/in/luke-mifflen/' target={'_blank'}>LinkedIn</a></p>
         <p>
-          (This is a sample website - built following the NextJS tutorial{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+          Hello I'm <bold>Luke</bold> A software developer from Calgary. You can
+          contact me on{" "}
+          <a href="https://www.linkedin.com/in/luke-mifflen/" target={"_blank"}>
+            LinkedIn
+          </a>
         </p>
+        <p>
+          (This is a sample website - built following the NextJS tutorial{" "}
+          <a href="https://nextjs.org/learn">Next.js tutorial</a>.)
+        </p>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   );
